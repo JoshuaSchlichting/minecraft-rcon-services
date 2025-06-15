5babfa540d805af3da0a1aa3b3d160e1da26ea90
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2025-06-15 16:05:18 -0400

    rm comments, only use long for hostname param

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 use clap::Parser;
 use rcon_client::{AuthRequest, AuthResponse, RCONClient, RCONConfig, RCONError, RCONRequest};
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
     #[arg(long)]
     password: String,
 
-    #[arg(short, long)]
+    #[arg(long)]
     hostname: String,
 
     #[arg(short = 'p', long)]
@@ -17,10 +15,8 @@ struct Args {
 }
 
 fn main() -> Result<(), RCONError> {
-    // Parse command line arguments
     let args = Args::parse();
 
-    // Create new RCON client
     let mut client = RCONClient::new(RCONConfig {
         url: format!("{}:{}", args.hostname, args.port),
         // Optional
@@ -28,7 +24,6 @@ fn main() -> Result<(), RCONError> {
         write_timeout: Some(37),
     })?;
 
-    // Auth request to RCON server (SERVERDATA_AUTH)
     let auth_result: AuthResponse = client.auth(AuthRequest::new(args.password))?;
     assert!(auth_result.is_success());
 
